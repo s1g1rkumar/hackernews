438e8b29e119de4a288f6f8d01c9567e21a63cce
sagar kumar rana <s1g1rk1m1r@gmail.com>
2021-04-07 16:33:58 +0530

    this is also initial version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React ,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+// function App() {
+//   return (
+//     <div className="App">
+     
+//     </div>
+//   );
+// }
+class App extends Component{
+  render() {
+    var helloworld = 'welcome';
+    return(
+      <div className = "App">
+        {helloworld}
+        <h1>this is first page</h1>
+        <h1>this is secound page</h1>
+        <h1>this is third page</h1>
+        <h1>this is fourth page</h1>
+        <h1>this is five page</h1>
+        <h1>this is six page</h1>
+        <h1>this is seven page</h1>
+
+        
+
+      
+       
+      </div>
+    );
+  }
+
+}   
+
+
 
 export default App;
