1b90aa910b56ec426fd7861fbbaff071f924510e
sagar kumar rana <s1g1rk1m1r@gmail.com>
2021-04-07 16:48:04 +0530

    version 1.10

--- src/App.js
+++ src/App.js
@@ -13,21 +13,33 @@ import React ,{Component} from 'react';
 class App extends Component{
   render() {
     var helloworld = 'welcome';
+    var page1 = '<h1>this is secund page</h1>';
+    var page2 = '<h1>this is third page</h1>';
+    var page3 = '<h1>this is fourth page</h1>';
+    var page4 = '<h1>this is fiveth page</h1>';
+    var page5 = '<h1>this is six page</h1>';
+    var page6 = '<h1>this is seven page</h1>';
+
+
     return(
       <div className = "App">
         {helloworld}
         <h1>this is first page</h1>
-        <h1>this is secound page</h1>
-        <h1>this is third page</h1>
-        <h1>this is fourth page</h1>
-        <h1>this is five page</h1>
-        <h1>this is six page</h1>
-        <h1>this is seven page</h1>
+        {page1}
+             <h2>tesing ... for marging project</h2>
+            
+        {page2}
 
-        
+        {page3}
 
-      
-       
+        {page4}
+
+        {page5}
+        {page6}
+
+
+
+
       </div>
     );
   }
